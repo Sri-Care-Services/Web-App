1ef4986ed9afbbed6b3028410a80ab0b760fa006
Thusikaran <pushparasa.thusikaran@gmail.com>
2024-09-30 21:47:48 +0530

    new updates

--- virtualr/src/components/Notification.jsx
+++ virtualr/src/components/Notification.jsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { getAllNotification } from '../service/notificationService';
+import moment from 'moment';
 
-const notifications = [
-  { id: 1, message: "Your Social Media Unlimited package has been activated!", timestamp: "2 minutes ago" },
-  { id: 2, message: "Your Streaming Package will expire in 3 days.", timestamp: "5 minutes ago" },
-  { id: 3, message: "You have used 80% of your data for the Gaming Package.", timestamp: "10 minutes ago" },
-  { id: 4, message: "New Family Package added! Check it out now.", timestamp: "15 minutes ago" },
-  { id: 5, message: "Your Business Package will renew automatically tomorrow.", timestamp: "20 minutes ago" },
-  { id: 6, message: "Limited time offer on the Unlimited Package!", timestamp: "30 minutes ago" },
-  { id: 7, message: "Your feedback on the Streaming Package has been received.", timestamp: "1 hour ago" },
-  { id: 8, message: "Congratulations! You’ve referred a friend to our service.", timestamp: "2 hours ago" },
-];
+
+// const notifications = [
+//   { id: 1, message: "Your Social Media Unlimited package has been activated!", timestamp: "2 minutes ago" },
+//   { id: 2, message: "Your Streaming Package will expire in 3 days.", timestamp: "5 minutes ago" },
+//   { id: 3, message: "You have used 80% of your data for the Gaming Package.", timestamp: "10 minutes ago" },
+//   { id: 4, message: "New Family Package added! Check it out now.", timestamp: "15 minutes ago" },
+//   { id: 5, message: "Your Business Package will renew automatically tomorrow.", timestamp: "20 minutes ago" },
+//   { id: 6, message: "Limited time offer on the Unlimited Package!", timestamp: "30 minutes ago" },
+//   { id: 7, message: "Your feedback on the Streaming Package has been received.", timestamp: "1 hour ago" },
+//   { id: 8, message: "Congratulations! You’ve referred a friend to our service.", timestamp: "2 hours ago" },
+// ];
+
 
 const Notifications = () => {
+  const [notifications,setNotifications]=useState(getAllNotification)
+
+  
+    //get all Notification
+    // const getAllNotifications = () => {
+      // getAllNotification
+      //     .then((response) => {
+      //         setPackages(response.data); 
+      //     })
+      //     .catch((error) => {
+      //         console.log(error);
+      //     });
+
+  // };
   return (
     
     <div className="max-w-[1200px] mx-auto p-4">
@@ -23,7 +41,7 @@ const Notifications = () => {
           >
             <div className="flex-1">
               <p className="text-gray-800">{notification.message} </p>
-              <span className="text-sm text-gray-400">{notification.timestamp}</span>
+              <span className="text-sm text-gray-400">{ moment(notification.createdAt).fromNow()}</span>
               
             </div>
           </div>
---
+++ virtualr/src/service/notificationService.js
@@ -0,0 +1,34 @@
+// export const getAllNotification = () => axios.get(BASE_URL+"/api/");
+
+export const  getAllNotification = [
+    {
+        "id": "66f06eaaa4537b035443d738",
+        "sender": "System",
+        "receiver": "user@example.com",
+        "status": "SENT",
+        "message": "Your payment of $100 has been received.",
+        "subject": "Payment Received",
+        "type": "PAYMENT",
+        "createdAt": "2024-09-26T00:53:22.2195226"
+    },
+    {
+        "id": "66f06eaaa4537b035443d738",
+        "sender": "System",
+        "receiver": "user@example.com",
+        "status": "SENT",
+        "message": "Your payment of $100 has been received.",
+        "subject": "Payment Received",
+        "type": "PAYMENT",
+        "createdAt": "2024-09-23T00:53:22.2195226"
+    },
+    {
+        "id": "66f06eaaa4537b035443d738",
+        "sender": "System",
+        "receiver": "user@example.com",
+        "status": "SENT",
+        "message": "Your payment of $100 has been received.",
+        "subject": "Payment Received",
+        "type": "PAYMENT",
+        "createdAt": "2024-09-23T00:53:22.2195226"
+    }
+]
